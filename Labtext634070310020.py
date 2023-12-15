2149465437b6a43fe3a9256db141546bce26451a
git-testlab <654070310093@npu.ac.th>
2023-12-15 15:02:38 +0700

    first commit

---
+++ Labtext634070310020.py
@@ -0,0 +1,6 @@
+print('Hello world')
+print('My name is Pramat Boonlakorn.')
+print('Mutt nickname')
+print('Graduated from secondary school. 6')
+print('From school/educational institution Nakhon Phanom Technical College')
+print('Nakhon Phanom Province')
